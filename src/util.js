618b61ad99572f2361a46a3593a21bdc0da67383
Michael Dougherty <maackle.d@gmail.com>
2017-12-09 19:37:34 -0800

    Fix getExcerpt

--- src/util.js
+++ src/util.js
@@ -9,7 +9,7 @@ export const postFilename = 'post.md'
 export const getExcerpt = content => {
   const len = 140;
   const excerpt = content.substring(0, 100);
-  if (excerpt.length < len - 3) {
+  if (content.length < len - 3) {
     return excerpt;
   } else {
     return excerpt + '...'
